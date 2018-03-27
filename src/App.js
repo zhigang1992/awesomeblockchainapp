a8e474760e79d0e1be9cb3a871a6bb5f6be304da
Zhigang Fang <zhigang1992@gmail.com>
2018-03-27 20:26:26 +0800

    Add metamask check

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+
+const web3 = global.web3 && new Web3(global.web3.currentProvider);
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  componentDidMount() {
+    if (!web3) { return }
+    web3.eth.getCoinbase((_, cb) => {
+      this.setState({coinbase: cb})
+      cb && web3.eth.getBalance(cb, (_, balance) => {
+        this.setState({balance: balance})
+      })
+    })
+  }
   render() {
+    if (!web3) {
+      return (
+        <p>Please install MetaMask here: <a href="https://metamask.io">https://metamask.io</a> </p>
+      )
+    }
+    const {coinbase, balance} = this.state
+    if (coinbase !== undefined && !coinbase) {
+      return (
+        <p>Please setup your metamask first</p>
+      )
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { coinbase && <p>You are {coinbase}</p>}
+        { balance && <p>Currently have {balance} </p>}
       </div>
     );
   }
